f77c8b8df0c035ed03a4a00114d0a281549a02c7
陈琼 <619591477@qq.com>
2025-03-01 06:37:03 +0800

    Update prepare_vscode.sh

--- prepare_vscode.sh
+++ prepare_vscode.sh
@@ -14,7 +14,9 @@ fi
 
 cp -f LICENSE vscode/LICENSE.txt
 
-cd vscode || { echo "'vscode' dir not found"; exit 1; }
+echo "hello 667788"
+
+cd vscode || { echo "'vscode hhjj8899' dir not found"; exit 1; }
 
 ../update_settings.sh
 
